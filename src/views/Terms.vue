56668d444a11323fe3228e512d28a28f0c40d863
katasu <katasu9923@gmail.com>
2019-02-18 12:26:46 +0900

    バーの実装

--- src/views/Terms.vue
+++ src/views/Terms.vue
@@ -1,11 +1,54 @@
 <template>
+
   <div id="terms">
-    <button>特に意味のないボタン</button>
-    <router-link :to="{ name: 'top' }">TOPに戻る</router-link>
-    <p>NEXT BUS {{ nHour }} : {{ nMin }}</p>
-    <p>PRE BUS {{ pHour }} : {{ pMin }}</p>
-    <p>NEXT NEXT BUS {{ nnHour }} : {{ nnMin }}</p>
-    <p>{{ lefHour }} : {{ lefMin }} : {{ lefSec }}</p>
+    <div class="header l-justify-center">
+      <div class="text">BUSBUS</div>
+    </div>
+    <div class="nav l-justify-center">
+      <span class="text">{{ pHour }} : {{ pMin }}</span>
+      <span class="text">・・・・・</span>
+      <span class="text">{{ nHour }} : {{ nMin }}</span>
+      <span class="text">・・・・・</span>
+      <span class="text">{{ nnHour }} : {{ nnMin }}</span>
+    </div>
+
+  <div class="l-justify-center u-mt8">
+    <div class="u-w80">
+
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 290 249">
+
+        <!--後ろの白いところ-->
+        <svg data-name="レイヤー" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 290 249">
+          <path d="M48.1 239a135 135 0 1 1 193.8 0" fill="none" stroke="#efefef" stroke-linecap="round"
+          stroke-miterlimit="10" stroke-width="20"/></svg>
+
+        <!--伸びる線-->
+        <g data-name="レイヤー 2">
+          <path class="logo" d="M48.1 239a135 135 0 1 1 193.8 0" fill="none" stroke="#16b2b2" stroke-linecap="round"
+            stroke-miterlimit="10" stroke-width="20" data-name="レイヤー 1"/>
+        </g>
+
+        <text x="90" y="110">バスが来るまで</text>
+        <text x="110" y="160">{{ lefHour }} : {{ lefMin }} : {{ lefSec }}</text>
+
+      </svg>
+      
+    </div>
+  </div>
+
+    <div class="l-justify-center u-mt8">
+      <ul id="dropmenu" contouchstart="">
+        <li><a href="#">行き先を選択</a>
+          <ul>
+            <li><router-link :to="{ name: 'top' }"><div class="l-justify-center">TOPに戻る</div></router-link></li>
+            <li><router-link :to="{ name: 'top' }"><div class="l-justify-center">TOPに戻る</div></router-link></li>
+            <li><router-link :to="{ name: 'top' }"><div class="l-justify-center">TOPに戻る</div></router-link></li>
+            <li><router-link :to="{ name: 'top' }"><div class="l-justify-center">TOPに戻る</div></router-link></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+
   </div>
 </template>
 
@@ -89,9 +132,9 @@ const doF = () => {
     getdoubleDigestNumer(getPre(hh)[1]),
     getdoubleDigestNumer(getNextNext(hh)[0]),
     getdoubleDigestNumer(getNextNext(hh)[1]),
-    getdoubleDigestNumer(getLeftMin()[0]),
-    getdoubleDigestNumer(getLeftMin()[1]),
-    getdoubleDigestNumer(getLeftMin()[2]),
+    getdoubleDigestNumer(getLeftTime()[0]),
+    getdoubleDigestNumer(getLeftTime()[1]),
+    getdoubleDigestNumer(getLeftTime()[2]),
   ];
 }
 
@@ -120,10 +163,10 @@ const getNextMin = (timeTable) => {
 }
 
 const getNextNext = (timeTable) => { //時刻表が引数
-  var nowNum = 0;
-  var nextNum = 0;
-  var nextNextHour = 0;
-  var nextNextMin = 0;
+  var nowNum;
+  var nextNum;
+  var nextNextHour;
+  var nextNextMin;
   const nHHMM = getNextMin(timeTable);
   const nextHour = nHHMM[0];
   const nextMin = nHHMM[1];
@@ -148,10 +191,10 @@ const getNextNext = (timeTable) => { //時刻表が引数
 
 //基本的にはgetNextNext()と同じ
 const getPre = (timeTable) => {
-  var nowNum = 0;
-  var preNum = 0;
-  var preHour = 0;
-  var preMin = 0;
+  var nowNum;
+  var preNum;
+  var preHour;
+  var preMin;
   const nHHMM = getNextMin(timeTable);
   const nextHour = nHHMM[0];
   const nextMin = nHHMM[1];
@@ -177,13 +220,14 @@ const getPre = (timeTable) => {
   return [preHour, preMin];
 }
 
-const getLeftMin = () => {
+const getLeftTime = () => {
   const nowHour = getNow()[0];
   const nowMin = getNow()[1];
   const nowSec = getNow()[2];
   const nextHour = getNextMin(hh)[0];
   const nextMin = getNextMin(hh)[1];
   var lefHour = nextHour - nowHour;
+  var lefSec = 59 - nowSec;
   if(lefHour < 0 ){
     lefHour = lefHour + 24;
   }
@@ -194,8 +238,6 @@ const getLeftMin = () => {
   } else if(lefMin != 0) {
     lefMin = lefMin - 1
   }
-  //ここまでで差分の時間と分が求まっている
-  var lefSec = 59 - nowSec;
   return [lefHour, lefMin, lefSec];
 }
 
@@ -227,3 +269,183 @@ return ("0" + number).slice(-2)
 }
 
 </script>
+
+<style lang="scss">
+
+.l-justify-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.u-w80 {
+  width: 70%;
+}
+
+.u-mt10 {
+  margin-top: 10vh;
+}
+
+.u-mt8 {
+  margin-top: 8vh;
+}
+
+.u-ab {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+}
+
+.header {
+  font-size: 24px;
+  background: #1D1D27;
+  color: white;
+  height: 5vh;
+  display: flex;
+}
+
+.nav {
+  background: #1D1D27;
+  color: white;
+  height: 8vh;
+  display: flex;
+  font-size: 16px;
+}
+
+circle {
+}
+
+#ccc {
+  stroke: lightgray;
+  fill: none;
+  stroke-width: 20;
+}
+
+#back {
+  fill: red;
+}
+
+@keyframes circle {
+  /* 直径*3.14 */
+  0% { stroke-dashoffset: 200; }
+  99.9%,to { stroke-dashoffset: 600; }
+}
+
+
+
+#dropmenu{
+  list-style-type: none;
+  width: 240px;
+  height: 50px;
+  margin: 5vh;
+  padding: 0;
+  background: #8a9b0f;
+  border-bottom: 5px solid #535d09;
+  border-radius: 30px 30px 30px 30px;
+}
+#dropmenu li{
+  position: relative;
+  width: 100%; /*メニューが一つなら100%*/
+  margin: 0;
+  padding: 0;
+  text-align: center;
+}
+#dropmenu li a{
+  display: block;
+  margin: 0;
+  padding: 15px 0 11px;
+  color: #fff;
+  font-size: 14px;
+  line-height: 1;
+  text-decoration: none;
+}
+#dropmenu li:hover > a{
+  background: #6e7c0c;
+  color: #eff7b1;
+}
+#dropmenu > li:hover > a{
+  border-radius: 30px 30px 0px 0px;
+}
+#dropmenu li ul{
+  list-style: none;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  margin: 0;
+  padding: 0;
+  border-radius: 30px 30px 30px 30px;
+}
+#dropmenu li:last-child ul{
+  width: 100%
+}
+#dropmenu li ul li{
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  color: #fff;
+  -moz-transition: .2s;
+  -webkit-transition: .2s;
+  -o-transition: .2s;
+  -ms-transition: .2s;
+  transition: .2s;
+}
+#dropmenu li ul li a{
+  padding: 13px 15px;
+  background: #6e7c0c;
+  text-align: left;
+  font-size: 12px;
+  font-weight: normal;
+}
+#dropmenu li:hover ul li{
+  overflow: visible;
+  height: 38px;
+  border-top: 1px solid #7c8c0e;
+  border-bottom: 1px solid #616d0b;
+}
+#dropmenu li:hover ul li:first-child{
+  border-top: 0;
+}
+#dropmenu li:hover ul li:last-child{
+  border-bottom: 0;
+}
+#dropmenu li:hover ul li:last-child a{
+  border-radius: 0px 0px 30px 30px;
+}
+
+#logo {
+
+}
+
+svg .logo{
+  fill: none;
+  stroke: #16B2B2; /*線の色を指定する*/
+  stroke-dasharray: 2000;/*線の間隔を指定する*/
+  stroke-dashoffset: 0;/*線の位置を指定する(IEは効かない属性)*/
+  stroke-width: 20;/*線の太さを指定する*/
+  -webkit-animation: hello 5s ease-in forwards;
+  animation: hello 5s ease-in forwards infinite;
+}
+
+ @-webkit-keyframes hello {
+
+}
+@keyframes hello {
+ 0% {
+ stroke-dashoffset: 2000;
+}
+ 30% {
+  stroke: #16B2B2;;
+ /*fill:transparent;*/ /*透過*/
+}
+ 40% {
+  stroke: #E84379;
+}
+ 100% {
+   stroke: #E84379;
+   stroke-dashoffset: 0;
+ /*fill:#333;*/
+}
+}
+
+
+</style>
