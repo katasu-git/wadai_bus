369b812e23588a108d0a59b71aaad2424e2c03b9
katasu <katasu9923@gmail.com>
2019-02-18 18:33:40 +0900

    バスの時間と人のアイコン表示

--- src/views/Terms.vue
+++ src/views/Terms.vue
@@ -4,16 +4,59 @@
     <div class="header l-justify-center l-100 u-fs150">
       <div class="text">BUSBUS</div>
     </div>
-    <div class="nav l-justify-center">
-      <span class="text l-030">{{ pHour }} : {{ pMin }}</span>
-      <span class="text l-030 u-mh1">・・・</span>
-      <span class="text l-100">{{ nHour }} : {{ nMin }}</span>
-      <span class="text l-070 u-mh1">・・・</span>
-      <span class="text l-070">{{ nnHour }} : {{ nnMin }}</span>
+    <div class="nav l-justify-center u-pt1 u-ph1">
+
+      <span class="">
+        <div class="l-012 u-tx-al-c">
+          {{ pHour }} : {{ pMin }}
+        </div>
+        <div class="u-fs060 u-tx-al-c l-000 u-mt1">
+          さらに次のバス
+        </div>
+      </span>
+
+      <span class="u-mh1">
+        <div class="l-012">
+          ・・・
+        </div>
+        <div class="u-fs060 u-tx-al-c l-000 u-mt1">
+          透明化
+        </div>
+      </span>
+
+      <span class="">
+        <div class="whitebox d-087 u-ph3">
+          {{ nHour }} : {{ nMin }}
+        </div>
+        <div class="u-fs060 u-tx-al-c l-100 u-mt1">
+          次のバス
+        </div>
+      </span>
+
+      <span class="u-mh1">
+          <div class="l-070">
+            ・・・
+          </div>
+          <div class="u-fs060 u-tx-al-c l-000 u-mt1">
+            透明化
+          </div>
+      </span>
+
+      <span class="">
+        <span class="u-tx-al-c">
+          <div class="l-070">
+            {{ nnHour }} : {{ nnMin }}
+          </div>
+          <div class="u-fs060 l-070 u-mt1">
+            さらに次のバス
+          </div>
+        </span>
+      </span>
+
     </div>
 
-  <div class="l-justify-center u-mt8">
-    <div class="u-w80">
+  <div class="l-justify-center u-mt10">
+    <div class="u-w060">
 
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 290 249">
 
@@ -27,15 +70,31 @@
             stroke-miterlimit="10" stroke-width="20" data-name="レイヤー 1"/>
         </g>
 
-        <text class="d-054 u-fs080" x="100" y="110">バスが来るまで</text>
-        <text class="d-087 u-fs200" x="90" y="160">{{ lefMin }} : {{ lefSec }}</text>
+        <text class="d-054 u-fs080" x="102" y="110">バスが来るまで</text>
+        <text class="d-087 u-fs250" x="90" y="160">{{ lefMin }} : {{ lefSec }}</text>
 
       </svg>
 
     </div>
   </div>
 
-    <div class="l-justify-center u-mt8">
+  <div class="l-justify-center">
+    <div class="u-w020 u-mt-5">
+      <svg class="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
+        <path class="walk" d="M129.71 51.34a18.67 18.67 0 1 0 19.45-17.85 18.68 18.68 0 0 0-19.45 17.85zM121.87 121c-.01 0-.05.09 0 0z"/>
+        <path class="walk" d="M90.45 148l15.16-34.28a9.83 9.83 0 0 1 .68-1.12 8.83 8.83 0 0 1 1.43-2.5s9-18.16 16.46-25.44c5.06-5.26 10.81-8.56
+              17.66-8.53 11.4.07 17.57 5.4 21 11l16.35 26.41 28 21.76a7.26 7.26 0 0 1-8.4 11.85L169.08 128c.29 7.82 9.89 127.82 9.89 127.82a9.47 9.47
+                0 0 1-18.78 2.53l-9-49.81-1.81 5.25a20.76 20.76 0 0 1-3.36 5.89l-29.9 31.72a8.52 8.52 0 1 1-12.26-11.84l23.67-34 8.31-33.41c-.07-5-8.91-56.87-8.91-56.87l-5 5.67s0-.08
+                0 0l-18.41 33.48a7.26 7.26 0 1 1-13-6.46z"/>
+        <path fill="none" d="M0 0h300v300H0z"/>
+      </svg>
+    </div>
+  </div>
+
+  <div class="l-justify-center u-mt1 d-087">まだ余裕</div>
+  <div class="l-justify-center u-mt1 d-054 u-fs080">次のバスは {{nHour}} : {{nMin}}</div>
+
+    <div class="l-justify-center u-mt18">
       <ul id="dropmenu" contouchstart="">
         <li><a href="#">行き先を選択</a>
           <ul>
@@ -114,14 +173,6 @@ const retTimeTable1 = () => {
 }
 
 const hh = retTimeTable1(); //時刻表の読み込み
-
-var test = "hey";
-
-const dotest = () => {
-  console.log(test);
-  test = "hi";
-  console.log(test);
-}
 
 const doF = () => {
   return [
@@ -270,4 +321,148 @@ return ("0" + number).slice(-2)
 </script>
 
 <style lang="scss">
+.header {
+  background: #1D1D27;
+  height: 7vh;
+}
+
+.nav {
+  background: #1D1D27;
+  height: 10vh;
+}
+
+.whitebox {
+  background: white;
+  border-radius: 20px;
+}
+
+#dropmenu{
+  list-style-type: none;
+  width: 240px;
+  height: 50px;
+  margin: 5vh;
+  padding: 0;
+  background: #16B2B2;
+  /*border-bottom: 5px solid #535d09;*/
+  border-radius: 30px 30px 30px 30px;
+}
+#dropmenu li{
+  position: relative;
+  width: 100%; /*メニューが一つなら100%*/
+  margin: 0;
+  padding: 0;
+  text-align: center;
+}
+#dropmenu li a{
+  display: block;
+  margin: 0;
+  padding: 15px 0 11px;
+  color: #fff;
+  font-size: 14px;
+  line-height: 1;
+  text-decoration: none;
+}
+#dropmenu li:hover > a{
+  background: #16B2B2;
+  color: #eff7b1;
+}
+#dropmenu > li:hover > a{
+  border-radius: 30px 30px 0px 0px;
+}
+#dropmenu li ul{
+  list-style: none;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  margin: 0;
+  padding: 0;
+  border-radius: 30px 30px 30px 30px;
+}
+#dropmenu li:last-child ul{
+  width: 100%
+}
+#dropmenu li ul li{
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  color: #fff;
+  -moz-transition: .2s;
+  -webkit-transition: .2s;
+  -o-transition: .2s;
+  -ms-transition: .2s;
+  transition: .2s;
+}
+#dropmenu li ul li a{
+  padding: 13px 15px;
+  background: #16B2B2;
+  text-align: left;
+  font-size: 12px;
+  font-weight: normal;
+}
+#dropmenu li:hover ul li{
+  overflow: visible;
+  height: 38px;
+  /*border-top: 1px solid #7c8c0e;
+  border-bottom: 1px solid #616d0b;*/
+}
+#dropmenu li:hover ul li:first-child{
+  border-top: 0;
+}
+#dropmenu li:hover ul li:last-child{
+  border-bottom: 0;
+}
+#dropmenu li:hover ul li:last-child a{
+  border-radius: 0px 0px 30px 30px;
+}
+
+svg .logo{
+  fill: none;
+  stroke: #16B2B2; /*線の色を指定する*/
+  stroke-dasharray: 2000;/*線の間隔を指定する*/
+  stroke-dashoffset: 0;/*線の位置を指定する(IEは効かない属性)*/
+  stroke-width: 20;/*線の太さを指定する*/
+  -webkit-animation: hello 5s ease-in forwards;
+  animation: hello 5s ease-in forwards infinite;
+}
+
+ @-webkit-keyframes hello {
+   0% {
+   stroke-dashoffset: 2000;
+  }
+   30% {
+    stroke: #16B2B2;
+   /*fill:transparent;*/ /*透過*/
+  }
+   40% {
+    stroke: #E84379;
+  }
+   100% {
+     stroke: #E84379;
+     stroke-dashoffset: 0;
+   /*fill:#333;*/
+  }
+}
+
+@keyframes hello {
+  0% {
+  stroke-dashoffset: 2000;
+ }
+  30% {
+   stroke: #16B2B2;
+  /*fill:transparent;*/ /*透過*/
+ }
+  40% {
+   stroke: #E84379;
+ }
+  100% {
+    stroke: #E84379;
+    stroke-dashoffset: 0;
+  /*fill:#333;*/
+ }
+}
+
+svg .walk {
+  fill: #16B2B2;
+}
+
 </style>
