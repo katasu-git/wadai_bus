5d8768c740ccd55e7b85d44c2250f57aa5a87286
katasu <katasu9923@gmail.com>
2019-04-30 13:43:17 +0900

    不要部分の削除

--- src/syllabus/views/Top.vue
+++ src/syllabus/views/Top.vue
@@ -1,49 +1,6 @@
 <template>
 <div id="s_top">
 
-  <!-- <div class="orange_area">
-      <div class="header">
-                  <p>WADAI</p>
-                  <p>SYLLABUS</p>
-              </div>
-              <div class="neko_wrapper">
-                  <img class="neko" alt="neko_img" src="../../assets/neko.png" v-on:click="goToBus" ontouchstart=""/>
-              </div>
-  </div>
-  <div class="container">
-    <a id="top"></a>
-      <div class="word_search">
-          <div class="searh_warpper">
-                  <div class="searchForm">
-                        <input v-on:keyup.enter="click" v-model="search" placeholder="授業の名前でさがす" class="searchForm-input" type="text">
-                            <svg  class="search_icon" v-on:click="click" ontouchstart="" version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" xml:space="preserve">
-                            <g>
-	                          <path d="M495.272,423.558c0,0-68.542-59.952-84.937-76.328c-24.063-23.938-33.69-35.466-25.195-54.931
-		                            c37.155-75.78,24.303-169.854-38.72-232.858c-79.235-79.254-207.739-79.254-286.984,0c-79.245,79.264-79.245,207.729,0,287.003
-		                            c62.985,62.985,157.088,75.837,232.839,38.691c19.466-8.485,31.022,1.142,54.951,25.215c16.384,16.385,76.308,84.937,76.308,84.937
-		                              c31.089,31.071,55.009,11.95,69.368-2.39C507.232,478.547,526.362,454.638,495.272,423.558z M286.017,286.012
-		                                c-45.9,45.871-120.288,45.871-166.169,0c-45.88-45.871-45.88-120.278,0-166.149c45.881-45.871,120.269-45.871,166.169,0
-	    	                            C331.898,165.734,331.898,240.141,286.017,286.012z">
-                            </path>
-                            </g>
-                            </svg>
-                  </div>
-              </div>
-          <div class="linkToBrank"><p>スクロールして空きコマ検索</p><p>↓</p></div>
-      </div>
-      <div class="brank_search">
-          <a href="#top"><div class="linkToName"><p>↑</p><p>スクロールして授業名検索</p></div></a>
-          <div class="calender_overflow">
-              <div class="calender_wrapper">
-                  <div class="grid_wrapper">
-                      <div class="grid" :style='{ color : activeColor(calendarNum), background : activeBackground(calendarNum) }' v-for="calendarNum in calendarNums" v-on:click="judgeNum(calendarNum)" ontouchstart="">{{ calendarNum }}</div>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div class="link_choicekoma">コマを選んでください</div>
-  </div> -->
-
   <div class="orange_area">
       <div class="header">
                   <p>WADAI</p>
